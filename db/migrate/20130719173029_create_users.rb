cae3e9090c3856c6333696fbce5262592a7f7749
chasm <charles.munat@gmail.com>
2013-07-19 11:49:47 -0700

    Added user migration comments.

--- db/migrate/20130719173029_create_users.rb
+++ db/migrate/20130719173029_create_users.rb
@@ -1,3 +1,16 @@
+# We created this migration and the User model with:
+#
+#   rails g model User name
+#
+# When we do "rake db:migrate", this migration will
+# create a users (note the plural) table in the database
+# with the following fields:
+#
+#   id          integer PRIMARY KEY
+#   name        character varying 255
+#   created_at  timestamp
+#   updated_at  timestamp
+#
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
