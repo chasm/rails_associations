8e9dccac84e02d9d7ff26180798ac2756a47bc31
chasm <charles.munat@gmail.com>
2013-07-19 11:52:22 -0700

    Added role migration comments.

--- db/migrate/20130719173113_create_roles.rb
+++ db/migrate/20130719173113_create_roles.rb
@@ -1,3 +1,16 @@
+# We created this migration and the Role model with:
+#
+#   rails g model Role name
+#
+# When we do "rake db:migrate", this migration will
+# create a roles (note the plural) table in the database
+# with the following fields:
+#
+#   id          integer PRIMARY KEY
+#   name        character varying 255
+#   created_at  timestamp
+#   updated_at  timestamp
+#
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
