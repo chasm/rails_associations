b8f88676caabb905dd3cb3f1e872b6183dfff6ff
chasm <charles.munat@gmail.com>
2013-07-19 11:50:48 -0700

    Added telephone migration comments.

--- db/migrate/20130719173103_create_telephones.rb
+++ db/migrate/20130719173103_create_telephones.rb
@@ -1,3 +1,19 @@
+# We created this migration and the Telephone model with:
+#
+#   rails g model Telephone num user:references
+#
+# When we do "rake db:migrate", this migration will
+# create a telphones (note the plural) table in the database
+# with the following fields:
+#
+#   id          integer PRIMARY KEY
+#   num         character varying 255
+#   user_id     integer, index: true
+#   created_at  timestamp
+#   updated_at  timestamp
+#
+# index: true adds an index to the user_id foreign key
+#
 class CreateTelephones < ActiveRecord::Migration
   def change
     create_table :telephones do |t|
