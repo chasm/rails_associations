15b56b7f0fea779240e532ea7755cdb27bdafe25
chasm <charles.munat@gmail.com>
2013-07-19 12:01:50 -0700

    Added comments to the seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,25 @@
+# Let's delete all the old roles and users. Because we have added
+# "dependent: :destroy" to the user-telephone association, deleting
+# the users will delete all associated telephones.
 Role.destroy_all
 User.destroy_all
 
+# Create and save three users.
 users = User.create([
   { name: "Chas." },
   { name: "Bono" },
   { name: "Enddy" }
 ])
 
+# Create and save three roles.
 roles = Role.create([
   { name: "Admin" },
   { name: "User" },
   { name: "Enddy" }
 ])
 
+# Associate the users with particular roles in a
+# many-to-many relationship.
 users[0].roles << [ roles[0], roles[2] ]
 users[1].roles << [ roles[1] ]
 users[2].roles << [ roles[0] ]
